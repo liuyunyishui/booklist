b544b77dc26b3e4e923dd8424d98f1485e3f461c
liuyun <liuyunhanxing@gmail.com>
2018-11-07 18:29:35 +0800

    a simple book list webpage

---
+++ src/main/java/readinglist/configuration/JdbcTemplateCondition.java
@@ -0,0 +1,22 @@
+package readinglist.configuration;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * Created by ly on 2018/11/7.
+ */
+public class JdbcTemplateCondition implements Condition{
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+        try {
+            conditionContext.getClassLoader().loadClass("org.springframework.jdbc.core.JdbcTemplate");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+
+    }
+
+}
